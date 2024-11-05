2169193cf62590cd8d17e68fad7ef0e7957708e9
Stiverson <stiverson.machado@gmail.com>
2024-11-05 17:56:06 -0300

    Ajustes criação do usuário e complementos da api

--- app.py
+++ app.py
@@ -3,13 +3,24 @@
 # Importação
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+CORS(app)
 
 # Modelagem
+# User (id, username, password)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password =db.Column(db.String(80), nullable=True)
+
+
 # Produto (id, name, price, description)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -75,8 +86,7 @@ def get_products():
          product_data = {
             "id": product.id,
             "name": product.name,
-            "price": product.price,
-            "description": product.description   
+            "price": product.price, 
         }
          product_list.append(product_data)
     return jsonify(product_list)
