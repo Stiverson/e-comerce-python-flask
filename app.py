3545f57b22fb460ac2e84c1156747faa47e64597
STIVERSON RODRIGUES <STIVERSON.RODRIGUES@AGROBILL.LOCAL>
2024-11-06 12:20:04 -0300

    Wip Construção da api implementando o carrinho de compras

--- app.py
+++ app.py
@@ -23,15 +23,28 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password =db.Column(db.String(80), nullable=True)
+    cart = db.relationship('CartItem', backref='user', lazy=True)
 
 
+# Produto (id, name, price, description)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    
+    
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    
+    
 # Autenticação
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
 
 @app.route('/login', methods=['POST'] )
 def login():
@@ -43,14 +56,7 @@ def login():
             login_user(user)
             return jsonify({"message": "Logged in successfully"})
     
-    return jsonify({"message": "Unauthorized. Invalid credentials"}), 401     
-
-# Produto (id, name, price, description)
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    description = db.Column(db.Text, nullable=True)
+    return jsonify({"message": "Unauthorized. Invalid credentials"}), 401 
 
 @app.route('/logout', methods=['POST'] )
 @login_required
