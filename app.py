4466f99d30c959acf4d89a2e85d27e604e514b25
tiagrobill <ti@agrobill.com.br>
2024-11-13 12:34:14 -0300

    wip continuação aula 4 final

--- app.py
+++ app.py
@@ -149,8 +149,50 @@ def add_to_cart(product_id):
     
     if user and product:
        cart_item = CartItem(user_id=user.id, product_id= product.id)
+       db.session.add(cart_item)
+       db.session.commit()
        return jsonify({'message': 'Item added to cart successfully'})
-    return jsonify({'message': 'Failed to cart successfully'}), 400
+    return jsonify({'message': 'Failed to add item to the cart'}), 400
 
+@app.route('/api/cart/remove/<int:product_id>', methods=["DELETE"])
+@login_required
+def remove_from_cart(product_id):
+    cart_item = CartItem.query.filter_by(user_id=current_user.id, product_id=product_id).first()
+    if cart_item:
+       db.session.delete(cart_item)
+       db.session.commit()
+       return jsonify({'message': 'Item removed from the cart successfully'})
+    return jsonify({'message': 'Failed to remove item from the cart'}), 400
+
+@app.route('/api/cart', methods=['GET'])
+@login_required
+def view_cart():
+ user = User.query.get(int(current_user.id))
+ cart_items = user.cart
+ cart_content = []
+ for cart_item in cart_items:
+     product = Product.query.get(cart_item.product_id)
+     cart_content.append({
+                                "id": cart_item.id,
+                                "user_id": cart_item.user_id,
+                                "product_id": cart_item.product_id,
+                                "product_name": product.name,
+                                "product_price": product.price
+                               
+ 
+     })
+
+ return jsonify(cart_content)
+
+@app.route('/api/cart/checkout', methods=["POST"])
+@login_required
+def checkout():
+     user = User.query.get(int(current_user.id))
+     cart_items = user.cart
+     for cart_item in cart_items:
+         db.session.delete(cart_item)
+     db.session.commit()
+     return jsonify({'message': 'Checkout successful. Cart has been cleared.'})
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
+
